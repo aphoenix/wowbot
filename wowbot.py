35e771d367b7e21cc9618bf93284579a5b958973
Andrew Phoenix <andrew.phoenix@gmail.com>
2015-11-27 21:53:47 +0000

    Updates to the help messages.

--- wowbot.py
+++ wowbot.py
@@ -21,7 +21,9 @@ handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(me
 logger.addHandler(handler)
 client = discord.Client()
 client.login(creds.discordid, creds.discordpw)
-
+user_agent = "Aphoenix's Discord / Reddit integration system."
+thing_limit = 25
+last_summons = time.time()
 
 with open('quotes.txt') as f:
     quotes = f.readlines()
@@ -29,6 +31,7 @@ with open('helpmessage.txt') as f:
     helpmessage = f.read()
 with open('rags.txt') as f:
     rags = f.readlines()
+
 roles = [
       'dk',
       'druid',
@@ -49,9 +52,38 @@ cannotset = """Sorry I could not set your class because I did not
             recognize the class you chose. Type `@WoWbot help` for
             more information."""
 
+
+r = praw.Reddit(user_agent=user_agent)
+r.login(creds.redditid,creds.redditpw) #replace with OAuth soon
+
+wowsub = r.get_subreddit('WoW')
+
+
 if not client.is_logged_in:
     print('Login fail')
     exit(1)
+
+
+def say_reddit_submission(submission,message):
+    saythis = submission.short_link
+    client.send_message(message.channel,saythis)
+
+
+def random_reddit_submission(subtype):
+    if subtype == 'hot':
+        submissions = wowsub.get_hot(limit = 27)
+    elif subtype == 'rising':
+        submissions = wowsub.get_rising(limit = 25)
+    elif subtype == 'new':
+        submissions = wowsub.get_new(limit = 25)
+    else:
+        print("Not a valid submission type")
+        return(False)
+    top_submissions = []
+    for sub in submissions:
+        top_submissions.append(sub)
+    random_submission = random.choice(top_submissions)
+    return(random_submission)
 
 
 @client.event
@@ -68,6 +100,7 @@ def on_ready():
 def on_message(message):
     if message.author.name == "aphoenix":
         if '!roles' in message.content.lower():
+            print(message.author.name + ': ' + message.content)
             for server in client.servers:
                 if server.name == 'wow':
                     wowroles = ''
@@ -79,6 +112,10 @@ def on_message(message):
                         wowroles += role.id
                         wowroles += '\n'
                     client.send_message(message.channel, wowroles[1:])
+    
+    if message.author.name == "SexualRhinoceros":
+        if '!butts' in message.content.lower():
+            client.send_message(message.channel, '<3')
 
 
     if '!help' in message.content.lower():
@@ -107,10 +144,35 @@ def on_message(message):
                 for role in server.roles:
                     if (myrole == role.name):
                         client.replace_roles(message.author,role)
+                        client.delete_message(message)
+   
+     
+    elif '!sticky' in message.content.lower():
+        if 'bottom' in message.content.lower():
+            sub = wowsub.get_sticky(bottom=True)
+        else:
+            sub = wowsub.get_sticky()
+        say_reddit_submission(sub, message)
+
+
+    elif '!hot' in message.content.lower():
+        sub = random_reddit_submission('hot')
+        say_reddit_submission(sub, message)
+
+
+    elif '!rising' in message.content.lower():
+        sub = random_reddit_submission('rising')
+        say_reddit_submission(sub, message)
+   
+   
+    elif '!new' in message.content.lower():
+        sub = random_reddit_submission('new')
+        say_reddit_submission(sub, message)
 
 
     elif '!rickme' in message.content.lower():
         client.send_message(message.channel, random.choice(quotes))
+
 
     elif '!purge' in message.content.lower():
         for server in client.servers:
@@ -120,10 +182,23 @@ def on_message(message):
                 saythis = ragnarquote + '\n'
                 saythis += purged.name + ' has been purged.'
                 client.send_message(message.channel,saythis)
-    elif '!butts' in message.content.lower():
-        client.send_message(message.channel,'SexyRhino likes big butts.')
-    elif '!wasthisdank' in message.content.lower():
-        client.send_message(message.channel,'No.')
+    
+
+    elif '!aphoenix' in message.content.lower():
+        global last_summons
+        time_passed = time.time() - last_summons 
+        print(time_passed)
+        if time_passed > 3600:
+            saythis = message.author.name + ' wants you in Discord'
+            try:
+                r.send_message('aphoenix', 'Discord Summons', saythis)
+                client.send_message(message.channel, 'Aphoenix has been summoned.')
+                last_summons = time.time()
+            except:
+                pass
+        else:
+            client.send_message(message.channel, 'You are doing too much aphoenix summoning.')
+            
     
 
 client.run()
